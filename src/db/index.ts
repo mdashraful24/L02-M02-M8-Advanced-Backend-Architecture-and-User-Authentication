e06060e7ecc043d149e78c07e33a7d460f2624e2
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-16 16:39:39 +0600

    create table using database qureay

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,11 +15,27 @@ export const initDB = async () => {
             password VARCHAR(20) NOT NULL,
             is_active BOOLEAN DEFAULT true,
             age INT,
+
             created_at TIMESTAMP DEFAULT NOW(),
             updated_at TIMESTAMP DEFAULT NOW()
             )
         `);
-        // console.log("Database connected successfully!");
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS profiles(
+            id SERIAL PRIMARY KEY,
+            user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+
+            bio TEXT,
+            address TEXT,
+            phone VARCHAR(15),
+            gender VARCHAR(10),
+            
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+        `)
+        console.log("Database connected successfully!");
     } catch (error) {
         console.log(error);
     }
