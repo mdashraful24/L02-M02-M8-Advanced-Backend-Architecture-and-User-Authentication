1e02eeed5a5ec28adab9994f273be5d689b9920d
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-17 00:17:54 +0600

    added understanding jwt and jwt based authentication

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Application, type Request, type Response } from "express";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
+import { authRoute } from "./modules/auth/auth.route";
 const app: Application = express();
 
 // Middleware
@@ -20,5 +21,6 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
+app.use("/api/auth", authRoute);
 
 export default app;
